0f89f17508e7052fa6438cecc3656c7ada35696a
20CE094 <97758311+20CE094@users.noreply.github.com>
2022-01-15 00:41:57 +0530

    Add files via upload

---
+++ pratical2_2_b.py
@@ -0,0 +1,8 @@
+#Name-Nehal Patel
+#ID- 20CE094
+#Github link: https://github.com/20CE094/Practical-2-2.b.git
+#Aim: Write a Python program to create a tuple with numbers and print one item.
+tuple=(1,2,3,4,5)
+i=int(input("Enter index: "))
+#accessing data value on index i from the tuple
+print("Value at index i is ",tuple[i])
